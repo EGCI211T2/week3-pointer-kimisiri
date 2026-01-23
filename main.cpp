01bb02d0605abe403967f73f16d76dfeb14b30dd
meh <siri.nga@student.mahidol.edu>
2026-01-23 11:22:10 +0700

    yay

--- main.cpp
+++ main.cpp
@@ -1,38 +1,34 @@
 #include <iostream>
 #include <iomanip>
-#define SIZE 10
+#include <cstdio>
 
 using namespace std;
 
 int main(int argc,char *argv[]){
-  int *pa, *pb, i, temp;
-int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
-pa = &a[0]; //pa =a;
-cout<<"Original: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
+  int n;
+  printf("How many number do u need > ");
+  scanf("%d", &n);
+  int* pa = new int[n];
 
-pa = &a[0];  
-pb = &a[SIZE-1];
-for (i=0; i<SIZE/2; i++)
-{
-   temp = *pa;  
-   *pa = *pb;  
-   *pb = temp;
-   pa++;  pb--;
-}
-pa=&a[0];
-pb-=SIZE/2;
-cout<<"Reversed: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
+  printf("Input this array : ");
+  for (int i = 0; i < n; i++) {
+    scanf("%d", pa+i);
+  }
 
+  printf("Your array : [");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", pa[i]);
+  }
+  printf("]\n");
 
-return 0;
+  printf("Your array reversed : [");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", pa[n-i-1]);
+  }
+  printf("]\n");
+
+  delete[] pa;
+  
+
+  return 0;
 }
