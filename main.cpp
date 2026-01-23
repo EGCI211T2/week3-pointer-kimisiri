78e2e64b4cdfb3613ef75d14e49e2560b08a665f
meh <siri.nga@student.mahidol.edu>
2026-01-23 11:33:11 +0700

    addon

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdio>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(int argc,char *argv[]){
-  int n;
+  int n = argc - 1; // ignore the command itself i guess 
+  /*int n;
   printf("How many number do u need > ");
-  scanf("%d", &n);
+  scanf("%d", &n);*/
   int* pa = new int[n];
 
-  printf("Input this array : ");
+  /*printf("Input this array : ");
   for (int i = 0; i < n; i++) {
     scanf("%d", pa+i);
+  }*/
+  for (int i = 1; i < n+1; i++) {
+    //printf("%d = %s\n", i, argv[i]);
+    *(pa+i-1) = atoi(argv[i]);
   }
 
   printf("Your array : ");
